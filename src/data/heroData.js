09347b1203196c6aa24563f62ae314f403fe7451
ramset <setiawanramadyan@gmail.com>
2021-06-17 11:26:08 +0700

    add herodata

--- src/data/heroData.js
+++ src/data/heroData.js
@@ -1,5 +1,9 @@
 import ImageOne from '../assets/kayon.jpg';
 import ImageTwo from '../assets/abasa.jpg';
+import ImageThree from '../assets/mirador.jpg';
+import ImageFour from '../assets/waikiki.jpg';
+import ImageFive from '../assets/borabora.jpg';
+import ImageSix from '../assets/alpine.jpg';
 
 export const heroData = [
   {
@@ -16,6 +20,38 @@ export const heroData = [
     path: '/resorts',
     label: 'View Resort',
     image: ImageTwo,
+    alt: 'Resort',
+  },
+  {
+    title: 'Mirador de Dalt Vila-Relais, Ibiza, Spain',
+    price: '$500',
+    path: '/resorts',
+    label: 'View Resort',
+    image: ImageThree,
+    alt: 'Resort',
+  },
+  {
+    title: 'Aston Waikiki Beach Resort, Hawaii, US',
+    price: '$900',
+    path: '/resorts',
+    label: 'View Resort',
+    image: ImageFour,
+    alt: 'Resort',
+  },
+  {
+    title: 'Four Seasons Resort Bora Bora, Bora Bora, Fr. Polynesia',
+    price: '$1800',
+    path: '/resorts',
+    label: 'View Resort',
+    image: ImageFive,
+    alt: 'Resort',
+  },
+  {
+    title: 'Alpine Design Resort, Grindelwald, Switzerland',
+    price: '$400',
+    path: '/resorts',
+    label: 'View Resort',
+    image: ImageSix,
     alt: 'Resort',
   },
 ];
